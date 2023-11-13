6eeb9d9e2de8764a7e901eb77291cbc6ff121080
bethanyang <148401145+bethanyang@users.noreply.github.com>
2023-11-13 11:45:02 -0800

    Update Shapes.cpp

--- Shapes.cpp
+++ Shapes.cpp
@@ -30,15 +30,44 @@ Shapes::Shapes(int color, int arr[20][10]) {
 	}
 }
 
-void Shapes::updatePosition(int arr[20][10]) {
-	
-}
 int Shapes::getColor() {
 	return this->color_;
 }
+//change horizontal position based on input
+void Shapes::updateHorizontalPosition(int arr[20][10], char letterEntered) {
+	bool ableToMove = true;
+	//if its g we are moving to the left 
+	if (letterEntered == 'g') {
+		for (int row = 0; row < positionLength_; row++) {
+			for (int col = 0; col < 2; col++) {
+				if (arr[coordinates_[row][col]][(coordinates_[row][col + 1]) - 1] == 0) {
+					ableToMove = true;//check if 
+				}
+				else{
+					break;
+				}
+			}
+		}
+		for (int r = 0; r < positionLength_; r++) {
+			for (int c = 0; c < 2; c++) {
+				//switches current value and value to the left on the grid
+				swap(arr[coordinates_[r][c]][coordinates_[r][c+1]], arr[coordinates_[r][c]][coordinates_[r][c - 1]]);
+			}
+		}
+	}
+	//check grid to see if x value -1 of coordinates = 0 
+	//if it is move x coordinates to left one which means - 1
+	//move grid coordinates r - 1 to equal 2 and change previous positions to 0
 
+	//create bool thats true as long as everything to left is always 0
+}
 //Each shape 
 void Shapes::colorOne(int arr[20][10]) {
+	arr[0][4] = 2;
+	arr[0][5] = 2;
+	arr[1][4] = 2;
+	arr[1][5] = 2;
+
 	this->coordinates_ [0][0] = 0;
 	this->coordinates_ [0][1] = 4;
 
@@ -56,11 +85,7 @@ void Shapes::colorOne(int arr[20][10]) {
 	1, 4
 	1, 5
 	*/
-	arr[0][4] = 2;
-	arr[0][5] = 2;
-	arr[1][4] = 2;
-	arr[1][5] = 2;
-	cout << "done";
+
 	//bar(amount from the left, start point from top, second amount from left, where the bottom is 
 	setcolor(YELLOW);
 	setfillstyle(SOLID_FILL, YELLOW);
@@ -202,15 +227,14 @@ void Shapes::colorSix(int arr[20][10]) {
 	this->coordinates_[3][0] = 2;
 	this->coordinates_[3][1] = 5;
 	/*
-	0, 5
+	0, 4
 	1, 4
 	1, 5
-	2, 4
+	2, 5
 	*/
 	//bar(amount from the left, start point from top, second amount from left, where the bottom is 
 	setcolor(BLUE);
 	setfillstyle(SOLID_FILL, BLUE);
 	bar(160, 0, 240, 80);
 }
-
 
