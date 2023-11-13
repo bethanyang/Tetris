7a9927211618b6d8cd8504e7f6ae9c52605e4cb0
bethanyang <148401145+bethanyang@users.noreply.github.com>
2023-11-13 10:29:03 -0800

    Update Shapes.cpp

--- Shapes.cpp
+++ Shapes.cpp
@@ -13,6 +13,9 @@ Shapes::Shapes(int color, int arr[20][10]) {
 	if (color_ == 1) {
 		colorOne(arr);
 	}
+	else if (color_ == 2) {
+		colorTwo(arr);
+	}
 }
 
 void Shapes::updatePosition(int arr[20][10]) {
@@ -49,10 +52,10 @@ void Shapes::colorOne(int arr[20][10]) {
 	1, 4
 	1, 5
 	*/
-	arr[0][4] = 1;
-	arr[0][5] = 1;
-	arr[1][4] = 1;
-	arr[1][5] = 1;
+	arr[0][4] = 2;
+	arr[0][5] = 2;
+	arr[1][4] = 2;
+	arr[1][5] = 2;
 	cout << "done";
 	//bar(amount from the left, start point from top, second amount from left, where the bottom is 
 	setcolor(YELLOW);
@@ -61,15 +64,39 @@ void Shapes::colorOne(int arr[20][10]) {
 
 }
 
-/*void Shapes::colorTwo(int arr[20][10]) {
-	arr[0][4] = 1;
-	arr[0][5] = 1;
-	arr[1][4] = 1;
-	arr[1][5] = 1;
+void Shapes::colorTwo(int arr[20][10]) {
+	arr[0][5] = 2;
+	arr[1][5] = 2;
+	arr[2][5] = 2;
+	arr[3][5] = 2;
 	//bar(amount from the left, start point from top, second amount from left, where the bottom is 
-	setcolor(YELLOW);
-	setfillstyle(SOLID_FILL, YELLOW);
+	setcolor(BLUE);
+	setfillstyle(SOLID_FILL, BLUE);
 	bar(160, 0, 240, 80);
 
+	//counter for how many squares it takes up 
+	this->positionLength_ = 4;
+
+	//creating memor for size of this array
+	coordinates_ = new int* [4];
+	for (int i = 0; i < 4; ++i) {
+		coordinates_[i] = new int[4];
+	}
+	this->coordinates_[0][0] = 0;
+	this->coordinates_[0][1] = 5;
+
+	this->coordinates_[1][0] = 1;
+	this->coordinates_[1][1] = 5;
+
+	this->coordinates_[2][0] = 2;
+	this->coordinates_[2][1] = 5;
+
+	this->coordinates_[3][0] = 3;
+	this->coordinates_[3][1] = 5;
+	/*
+	0, 5
+	1, 5
+	2, 5
+	3, 5
+	*/
 }
-*/
